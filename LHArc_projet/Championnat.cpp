28ef5a8d4cc675f156fc3a6d41485131c0abf03b
Thomas Frantzen <thomas.frantzen@he-arc.ch>
2018-04-26 16:41:28 +0200

    Définition du constructeur, AjouterEquipe, AgenderMatch, Réinitialiser

--- LHArc_projet/Championnat.cpp
+++ LHArc_projet/Championnat.cpp
@@ -4,22 +4,22 @@ using namespace std;
 
 Championnat::Championnat(string nom)
 {
-
+    this->nom = nom;
 }
 
 Championnat::~Championnat()
 {
-
+ //dstr
 }
 
 void Championnat::AjouterEquipe(Equipe equipe)
 {
-
+    listEquipe.push_front(equipe);
 }
 
 void Championnat::AgenderMatch(Match match)
 {
-
+    listMatches.push_front(match);
 }
 
 void Championnat::AfficherEquipe()
@@ -39,5 +39,6 @@ void Championnat::JouerMatch(short t1, short t2)
 
 void Championnat::Reinitialiser()
 {
-
+    listEquipe.clear();
+    listMatches.clear();
 }
