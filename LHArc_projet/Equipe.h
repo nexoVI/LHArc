2ad3beadd9fb57fc04a44f7355b0bb865c06e779
Valérian Glardon <valerian.glardon@he-arc.ch>
2018-04-26 10:14:02 +0200

    Rajoute classes Equipe, Match, Championnat (vides)

---
+++ LHArc_projet/Equipe.h
@@ -0,0 +1,16 @@
+#ifndef EQUIPE_H
+#define EQUIPE_H
+
+
+class Equipe
+{
+    public:
+        Equipe();
+        ~Equipe();
+
+    protected:
+
+    private:
+};
+
+#endif // EQUIPE_H
---
+++ LHArc_projet/Match.h
@@ -0,0 +1,15 @@
+#ifndef MATCH_H
+#define MATCH_H
+
+
+class Match
+{
+    public:
+        Match();
+
+    protected:
+
+    private:
+};
+
+#endif // MATCH_H
